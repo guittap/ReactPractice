f620351c19eccd7096b48ec5f0d504e96472fd12
wargen <wargenng@gmail.com>
2020-01-18 21:15:54 -0800

    creating new component with array data

--- react-for-web-designers/Ex_Files_React_Designers/Exercise Files/Ch02/wip/start/app.js
+++ react-for-web-designers/Ex_Files_React_Designers/Exercise Files/Ch02/wip/start/app.js
@@ -1,6 +1,26 @@
 (function() {
   "use strict";
 
+  function SizeSelector(props) {
+    function sizeOptions() {
+      const sizes = window.Inventory.allSizes;
+      return sizes.map(function(num) {
+        return (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        );
+      });
+    }
+    return (
+      <div className="field-group">
+        <label htmlFor="size-options">Size:</label>
+        <select name="sizeOptions" id="size-options">
+          {sizeOptions()}
+        </select>
+      </div>
+    );
+  }
   function ProductImage(props) {
     return <img src="../../../assets/red.jpg" alt="Product image" />;
   }
@@ -11,6 +31,10 @@
         <div className="product-image">
           <ProductImage />
         </div>
+        <div className="selectors">
+          <SizeSelector />
+        </div>
+        >
       </div>
     );
   }
