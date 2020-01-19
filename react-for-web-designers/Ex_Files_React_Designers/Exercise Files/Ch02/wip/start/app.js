a6543502e18c4eba91f9156c93b205b6275b784d
wargen <wargenng@gmail.com>
2020-01-18 20:10:11 -0800

    installing react in an existing site

--- react-for-web-designers/Ex_Files_React_Designers/Exercise Files/Ch02/wip/start/app.js
+++ react-for-web-designers/Ex_Files_React_Designers/Exercise Files/Ch02/wip/start/app.js
@@ -3,4 +3,11 @@
 
   // Start here
 
+  const ProductCostomizer = React.createElement(
+    "div",
+    { className: "cumstomizer" },
+    "Product customizer will go here"
+  );
+
+  ReactDOM.render(ProductCostomizer, document.getElementById("react-root"));
 })();
