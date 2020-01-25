462679ee45fe42c86c61c4d9c835bab13dd76e07
wargen <wargenng@gmail.com>
2020-01-24 22:04:53 -0800

    scaffold initial components

--- react-for-web-designers/Ex_Files_React_Designers/Exercise Files/Ch03/wip/start/directory.js
+++ react-for-web-designers/Ex_Files_React_Designers/Exercise Files/Ch03/wip/start/directory.js
@@ -1,4 +1,31 @@
 (function() {
   "use strict";
 
+  class Directory extends React.Component{
+    constructor(props) {
+      super(props);
+
+      this.state = (
+        people = window.LMDirectory.people
+      );
+
+    }
+
+    render() {
+      return (
+        <div className="company-directory">
+        <h2>
+						Company Directory
+				</h2>
+				<p>Learn more about each person at Leaf & Mortar in this company directory.</p>
+        </div>
+
+        <Filters />
+
+        <People people={this.state.people}/>
+      );
+    }
+  }
+
+  ReactDOM.render(<Directory />, document.getElementById("directory-root"));
 })();
